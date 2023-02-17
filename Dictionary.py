15e211ffd4ba551ddb63dfff9f5253196d49e478
cabaelamirxle <amirxlecabael28@gmail.com>
2023-02-17 19:00:05 +0800

    functional choices

--- Dictionary.py
+++ Dictionary.py
@@ -3,23 +3,43 @@ from difflib import get_close_matches
 
 # reading the json file containing words and its meaning
 data = json.load(open('words.json'))
-  
-# translate the word in lowercase, since all the words in json file is in lower case.
-word = input('Enter a word: ').lower()
-    
-# give three possible words similar to wrong input
-similar = get_close_matches(word, data.keys())
-      
-def translate(possible):
-    if possible in data:
-        return data[possible]
-    elif len(similar) > 0:
-        suggested = input(f"word not found! try these {similar}: ").lower() #translate input in lowercase
-        return data[suggested]
-    else:
-        return "Word doesn't exist!"
 
-print(translate(word))
+#function 1
+def displayWords():
+    letter = input("Enter a letter from a-z: ")
+    for i in data:
+        if i.startswith(letter):
+            print(i)
+
+#function 2:
+def specificWord():
+    # translate the word in lowercase, since all the words in json file is in lower case.
+    word = input('Enter a word: ').lower()
+        
+    # give three possible words similar to wrong input
+    similar = get_close_matches(word, data.keys())
+        
+    def translate(possible):
+        if possible in data:
+            return data[possible]
+        elif len(similar) > 0:
+            suggested = input(f"word not found! try these {similar}: ").lower() #translate input in lowercase
+            return data[suggested]
+        else:
+            return "Word doesn't exist!"
+
+    print(translate(word))
+    
+#function 3
+def specificNumber():
+    num = int(input("Enter number of length of the word: "))
+    found = False
+    for key in data:
+        if len(key)==num:
+            print(key)
+            found = True
+    if found == False:
+        print("No words with specific length")
 
 #Choices
 while True:
