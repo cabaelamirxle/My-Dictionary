2e38ed768c6d90d864cfa650567ee4faca83819e
cabaelamirxle <amirxlecabael28@gmail.com>
2023-02-17 16:09:27 +0800

    adjusting 1 done

--- Dictionary.py
+++ Dictionary.py
@@ -7,5 +7,15 @@ data = json.load(open('words.json'))
 word = input('Enter a word: ').lower()
     
 # give three possible words similar to wrong input
-possible = get_close_matches(word, data.keys())
+similar = get_close_matches(word, data.keys())
       
+def translate(possible):
+    if possible in data:
+        return data[possible]
+    elif len(similar) > 0:
+        suggested = input(f"word not found! try these {similar}: ").lower() #translate input in lowercase
+        return data[suggested]
+    else:
+        return "Word doesn't exist!"
+
+print(translate(word))
